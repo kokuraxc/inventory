4b5a8a325f091f6ef4d130f3a0acd8d96aa89280
Guo Zhengjin <guo.zhengjin@gmail.com>
2020-02-17 21:51:35 +0800

    added splash scren

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inventory/splashscreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,9 +19,10 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashScreen(),
+      // home: MyHomePage(title: 'Inentory Home'),
     );
   }
 }
---
+++ lib/splashscreen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:inventory/overview.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // redirect to overview screen
+    Future.delayed(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => OverviewScreen())));
+
+    return Material(
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [
+              0.0,
+              0.3,
+              0.7,
+              1.0,
+            ],
+                colors: [
+              Colors.green,
+              Colors.blue,
+              Colors.orange,
+              Colors.pink
+            ])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: Image.asset('images/artclogo.png'),
+              ),
+              Text(
+                "SMBA-Inventory",
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SpinKitRipple(color: Colors.yellow),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
